869e71a4bd4011ef38950cbd3e519ba1d6d9e59b
Eldorado16 <65019139+Eldorado16@users.noreply.github.com>
2023-08-11 03:34:26 -0700

    Delete 0x03-ES6_data_manipulation directory

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++
@@ -1,9 +0,0 @@
-// creat a function named getListStudents that returns an array of objects
-export default function getListStudents() {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return students;
-}
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++
@@ -1,6 +0,0 @@
-export default function getStudentIdsSum(students) {
-  if (Array.isArray(students) === false) {
-    return [];
-  }
-  return students.reduce((accumulator, student) => accumulator + student.id, 0);
-}
